9fa37308517d033aefa81fa89ae4baa6fd15741b
Oscar Curto <oscarcj98@gmail.com>
2022-10-09 18:27:44 +0200

    Añadidas querys y test de account

--- homework3.sql
+++ homework3.sql
@@ -1,3 +1,12 @@
 DROP SCHEMA IF EXISTS homework3;
 CREATE SCHEMA homework3;
 use homework3;
+SELECT AVG(employee_count) FROM account;
+SELECT MAX(employee_count) from account;
+SELECT MIN(employee_count) from account;
+
+
+SELECT AVG(opportunity.id), account_id from opportunity join account on opportunity.account_id = account.id group by account.id;
+SELECT MAX(opportunity.id), account_id from opportunity join account on opportunity.account_id = account.id group by account.id;
+SELECT MIN(opportunity.id), account_id from opportunity join account on opportunity.account_id = account.id group by account.id;
+SELECT AVG(opportunity.account_id) FROM opportunity GROUP BY opportunity.account_id;
--- src/main/java/com/ironhack/Homework3/repositories/AccountRepository.java
+++ src/main/java/com/ironhack/Homework3/repositories/AccountRepository.java
@@ -2,8 +2,26 @@ package com.ironhack.Homework3.repositories;
 
 import com.ironhack.Homework3.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface AccountRepository extends JpaRepository<Account,Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    //The mean employeeCount can be displayed by typing “Mean EmployeeCount”
+    @Query("SELECT AVG(employeeCount) FROM Account")
+    BigDecimal meanEmployeeCount();
+
+    //The maximum employeeCount can be displayed by typing “Max EmployeeCount”
+    @Query("SELECT MAX(employeeCount) FROM Account")
+    Long maxEmployeCount();
+
+    //The minimum employeeCount can be displayed by typing “Min EmployeeCount”
+    @Query("SELECT MIN(employeeCount) FROM Account")
+    Long minEmployeCount();
+
 }
